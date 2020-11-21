de1bf628a9d470cf9702292b28c8f68b27cb206d
Alper Kaan YILDIZ <cs.se.aky@gmail.com>
2020-11-21 16:12:33 +0300

    Styling for conference creation form.

--- src/resources/views/conferences/index.blade.php
+++ src/resources/views/conferences/index.blade.php
@@ -2,93 +2,93 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-yellow-400 p-6 rounded-lg">
+        <div class="w-5/12 bg-gradient-to-r from-teal-400 to-blue-500 p-6 rounded-lg">
             <form action="{{ route('conference') }}" method="post">
                 @csrf
                 <div class="mb-4">
-                    <label for="CreationDateTime">Choose conference creation time</label>
-                    <input type="datetime-local" name="CreationDateTime" class="js-datepicker border-4 border-indigo-600">
-                </div>
-
-                @error('CreationDateTime')
-                <div class="text-red-600 mt-2 text-sm">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <div class="mb-4">
-                    <label for="Name">Conference Name</label>
-                    <input type="text" name="Name" class="border-4  p-4 rounded-lg">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="Name">Conference Name</label>
+                    <input class="border-4 border-pink-800" type="text" name="Name">
                 </div>
 
                 @error('Name')
-                <div class="text-red-600 mt-2 text-sm">
+                <div class="text-pink-900	 mt-2 text-sm">
                     {{ $message }}
                 </div>
                 @enderror
 
                 <div class="mb-4">
-                    <label for="ShortName">Conference Short Name</label>
-                    <input type="text" name="ShortName">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="ShortName">Conference Short Name</label>
+                    <input class="border-4 border-pink-800" type="text" name="ShortName">
                 </div>
 
                 @error('ShortName')
-                <div class="text-red-600 mt-2 text-sm">
+                <div class="text-pink-900	 mt-2 text-sm">
                     {{ $message }}
                 </div>
                 @enderror
 
                 <div class="mb-4">
-                    <label for="Year">Conference Year</label>
-                    <input type="number" name="Year">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="Year">Conference Year</label>
+                    <input class="border-4 border-pink-800" type="number" name="Year">
                 </div>
 
                 @error('Year')
-                <div class="text-red-600 mt-2 text-sm">
+                <div class="text-pink-900	 mt-2 text-sm">
                     {{ $message }}
                 </div>
                 @enderror
 
                 <div class="mb-4">
-                    <label for="StartDate">Choose conference start date</label>
-                    <input type="date" name="StartDate" class="js-datepicker border-4 border-indigo-600">
-                </div>
-
-                @error('StartDate')
-                <div class="text-red-600 mt-2 text-sm">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <div class="mb-4">
-                    <label for="EndDate">Choose conference end date</label>
-                    <input type="date" name="EndDate" class="js-datepicker border-4 border-indigo-600">
-                </div>
-
-                @error('EndDate')
-                <div class="text-red-600 mt-2 text-sm">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <div class="mb-4">
-                    <label for="Submission_Deadline">Choose submission deadline</label>
-                    <input type="date" name="Submission_Deadline" class="js-datepicker border-4 border-indigo-600">
-                </div>
-
-                @error('Submission_Deadline')
-                <div class="text-red-600 mt-2 text-sm">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <div class="mb-4">
-                    <label for="WebSite">Conference website</label>
-                    <input type="text" name="WebSite">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="WebSite">Conference website</label>
+                    <input class="border-4 border-pink-800" type="text" name="WebSite">
                 </div>
 
                 @error('WebSite')
-                <div class="text-red-600 mt-2 text-sm">
+                <div class="text-pink-900	 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="CreationDateTime">Choose conference creation time</label>
+                    <input type="datetime-local" name="CreationDateTime" class="border-4 border-indigo-600">
+                </div>
+
+                @error('CreationDateTime')
+                <div class="text-pink-900	 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="StartDate">Choose conference start date</label>
+                    <input type="date" name="StartDate" class="border-4 border-indigo-600">
+                </div>
+
+                @error('StartDate')
+                <div class="text-pink-900	 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="EndDate">Choose conference end date</label>
+                    <input type="date" name="EndDate" class="border-4 border-indigo-600">
+                </div>
+
+                @error('EndDate')
+                <div class="text-pink-900	 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-1" for="Submission_Deadline">Choose submission deadline</label>
+                    <input type="date" name="Submission_Deadline" class="border-4 border-indigo-600">
+                </div>
+
+                @error('Submission_Deadline')
+                <div class="text-pink-900	 mt-2 text-sm">
                     {{ $message }}
                 </div>
                 @enderror
@@ -99,4 +99,5 @@
             </form>
         </div>
     </div>
+
 @endsection
