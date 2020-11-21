2be24cad302afaeaeeca24d514d54b6128d7a40a
Alper Kaan YILDIZ <cs.se.aky@gmail.com>
2020-11-21 16:21:11 +0300

    Added SQL script for creating conference table.

---
+++ SQLScripts/create_conference_table.sql
@@ -0,0 +1,15 @@
+create table Conference
+(
+    CreationDateTime    datetime  not null,
+    Name                char(100) not null,
+    ShortName           char(19)  not null,
+    Year                int       not null,
+    StartDate           datetime  not null,
+    EndDate             datetime  not null,
+    Submission_Deadline datetime  not null,
+    WebSite             char(100) not null,
+    ConfID              varchar(20) as (concat(`ShortName`, '_', `Year`)) stored
+        primary key
+)
+    collate = utf8mb4_unicode_ci;
+
