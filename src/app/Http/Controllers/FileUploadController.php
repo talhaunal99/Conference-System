dd62af76e8259a0024aa901bf747428154f7bdd6
talhaunal <t.unal@etu.edu.tr>
2020-11-28 02:38:07 +0300

    Required file type changed to .pdf

--- src/app/Http/Controllers/FileUploadController.php
+++ src/app/Http/Controllers/FileUploadController.php
@@ -12,7 +12,7 @@ class FileUploadController extends Controller{
 
     public function fileUpload(Request $req){
         $req->validate([
-            'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048'
+            'file' => 'required|pdf|max:2048'
         ]);
 
         $fileModel = new File;
