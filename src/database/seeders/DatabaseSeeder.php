05c90082ffd33b245a8b150b031d0c5e7e5f80dd
Alper Kaan YILDIZ <cs.se.aky@gmail.com>
2020-11-30 11:53:35 +0300

    Adding usersinfo seeds.

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -86,6 +86,17 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
+            'id' => 1,
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123'),
+            'approved' => 1,
+            'role' => 'Admin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
             'id' => 2,
             'username' => 'talha',
             'email' => 'talha@gmail.com',
@@ -98,9 +109,9 @@ class DatabaseSeeder extends Seeder
 
         DB::table('users')->insert([
             'id' => 3,
-            'username' => 'kaan',
-            'email' => 'kaan@gmail.com',
-            'password' => Hash::make('kaan123'),
+            'username' => 'alper',
+            'email' => 'alper@alper.com',
+            'password' => Hash::make('alper123'),
             'approved' => 1,
             'role' => 'Admin',
             'created_at' => date('Y-m-d H:i:s'),
@@ -119,30 +130,48 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
-            'id' => 4,
+            'id' => 5,
             'username' => 'tansel',
             'email' => 'tansel@gmail.com',
             'password' => Hash::make('tansel123'),
-            'approved' => 1,
+            'approved' => 0,
             'role' => 'User',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
 
         DB::table('users')->insert([
-            'id' => 4,
+            'id' => 6,
             'username' => 'busra',
             'email' => 'busra@gmail.com',
             'password' => Hash::make('busra123'),
-            'approved' => 1,
+            'approved' => 0,
             'role' => 'User',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
 
+        DB::table('UsersInfo')->insert([
+            'id' => 1,
+            'Salutation' => 'Admin',
+            'Name' => 'Admin of',
+            'LastName' => 'the website',
+            'Affiliation' => 'Head of website development at Website University',
+            'SecondaryEmail' => 'admin2@gmail.com',
+            'Phone' => '05059594563',
+            'Fax' => '+44 161 999 8881',
+            'URL' => 'http://www.admin.com/website/developer',
+            'Address' => 'Admin street, Website province.',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'CountryCode' => 'TUR',
+            'CityId' => 1,
+            'AuthenticationID' => 1,
+        ]);
+
         DB::table('UsersLog')->insert([
             'id' => 2,
-            'Salutation' => 'busra',
+            'Salutation' => 'Mr.',
             'Name' => 'Talha',
             'LastName' => 'Ünal',
             'Affiliation' => 'Medical Informatics Group School of Biotechnology Dublin City University',
@@ -156,6 +185,78 @@ class DatabaseSeeder extends Seeder
             'CountryCode' => 'TUR',
             'CityId' => 1,
             'AuthenticationID' => 2,
+        ]);
+
+        DB::table('UsersLog')->insert([
+            'id' => 2,
+            'Salutation' => 'Mr.',
+            'Name' => 'Alper Kaan',
+            'LastName' => 'YILDIZ',
+            'Affiliation' => 'Medical Informatics Group School of Biotechnology Dublin City University',
+            'SecondaryEmail' => 'alper2@gmail.com',
+            'Phone' => '05059594565',
+            'Fax' => '+44 161 999 8882',
+            'URL' => 'http://www.csseaky.com/believe/bit?bells=boundary&actor=acoustics',
+            'Address' => 'Emek, Çankaya',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'CountryCode' => 'TUR',
+            'CityId' => 1,
+            'AuthenticationID' => 3,
+        ]);
+
+        DB::table('UsersLog')->insert([
+            'id' => 3,
+            'Salutation' => 'Mr.',
+            'Name' => 'Yahya Can',
+            'LastName' => 'TUĞRUL',
+            'Affiliation' => 'Medical Informatics Group School of Biotechnology Dublin City University',
+            'SecondaryEmail' => 'yahya2@gmail.com',
+            'Phone' => '050592594565',
+            'Fax' => '+44 161 999 88222',
+            'URL' => 'http://www.yct.com/believe/bit?bells=boundary&actor=acoustics',
+            'Address' => 'Demetevler, Yenimahalle',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'CountryCode' => 'TUR',
+            'CityId' => 1,
+            'AuthenticationID' => 4,
+        ]);
+
+        DB::table('UsersLog')->insert([
+            'id' => 4,
+            'Salutation' => 'Mr.',
+            'Name' => 'Tansel',
+            'LastName' => 'ÖZYER',
+            'Affiliation' => 'Computer Science Department',
+            'SecondaryEmail' => 'ozyer@etu.edu.tr',
+            'Phone' => '0505925942565',
+            'Fax' => '+44 161 9199 88222',
+            'URL' => 'http://ozyer.etu.edu.tr',
+            'Address' => 'Söğütözü, ÇANKAYA',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'CountryCode' => 'TUR',
+            'CityId' => 1,
+            'AuthenticationID' => 5,
+        ]);
+
+        DB::table('UsersLog')->insert([
+            'id' => 4,
+            'Salutation' => 'Mr.',
+            'Name' => 'Tansel',
+            'LastName' => 'ÖZYER',
+            'Affiliation' => 'Computer Science Department',
+            'SecondaryEmail' => 'ozyer@etu.edu.tr',
+            'Phone' => '0505925942565',
+            'Fax' => '+44 161 9199 88222',
+            'URL' => 'http://ozyer.etu.edu.tr',
+            'Address' => 'Söğütözü, ÇANKAYA',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'CountryCode' => 'TUR',
+            'CityId' => 1,
+            'AuthenticationID' => 5,
         ]);
 
         /*
