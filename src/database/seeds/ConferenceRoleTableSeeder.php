b57fb465db2d79b2e96761709a176ab707dd3bce
talhaunal <t.unal@etu.edu.tr>
2020-11-30 14:03:40 +0300

    ConferenceTableSeeder created for 2 Conferences

---
+++ src/database/seeds/ConferenceRoleTableSeeder.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConferenceRoleTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('ConferenceRole')->delete();
+        
+        \DB::table('ConferenceRole')->insert(array (
+            0 => 
+            array (
+                'ConfID' => 'flog_2020',
+                'Role' => 'Chair',
+                'AuthenticationID' => 2,
+            ),
+            1 => 
+            array (
+                'ConfID' => 'jone_2021',
+                'Role' => 'Chair',
+                'AuthenticationID' => 2,
+            ),
+            2 => 
+            array (
+                'ConfID' => 'flog_2020',
+                'Role' => 'Chair',
+                'AuthenticationID' => 3,
+            ),
+            3 => 
+            array (
+                'ConfID' => 'jone_2021',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 3,
+            ),
+            4 => 
+            array (
+                'ConfID' => 'flog_2020',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 4,
+            ),
+            5 => 
+            array (
+                'ConfID' => 'jone_2021',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 4,
+            ),
+            6 => 
+            array (
+                'ConfID' => 'flog_2020',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 5,
+            ),
+            7 => 
+            array (
+                'ConfID' => 'jone_2021',
+                'Role' => 'Chair',
+                'AuthenticationID' => 5,
+            ),
+            8 => 
+            array (
+                'ConfID' => 'flog_2020',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 6,
+            ),
+            9 => 
+            array (
+                'ConfID' => 'jone_2021',
+                'Role' => 'Reviewer',
+                'AuthenticationID' => 6,
+            ),
+        ));
+        
+        
+    }
+}
