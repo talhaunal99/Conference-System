44513d7f2fa05a0bfee8138e7487f496183cc31e
talhaunal <t.unal@etu.edu.tr>
2020-11-30 14:01:45 +0300

    DatabaseSeeder updated for 2 Conferences

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -16,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //
+        $this->call(ConferenceTableSeeder::class);
+        $this->call(ConferenceRoleTableSeeder::class);
+        $this->call(ConferenceTagTableSeeder::class);
     }
 }
